bebc160a7cd09dff0dbd2dfa84f9ed25fb198563
Judisson <judissonDev@hotmail.com>
2024-07-03 14:30:25 -0300

    Create - Usando o Chat GPT para tradução da sinopse

---
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obterTraducao(String texto) {
+        System.out.println("Texto para traduzir: " + texto);
+        OpenAiService service = new OpenAiService("chave aqui");
+
+        CompletionRequest requisicao = CompletionRequest.builder()
+                .model("gpt-3.5-turbo-instruct")
+                .prompt("traduza para o português o texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        System.out.println("Requisição: ");
+        System.out.println(requisicao);
+        System.out.println();
+
+        var resposta = service.createCompletion(requisicao);
+        return resposta.getChoices().get(0).getText();
+    }
+}
