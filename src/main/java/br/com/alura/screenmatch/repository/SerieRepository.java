4e796845dbf25ead0ce25f1ba233314a2e98eefb
Judisson <judissonDev@hotmail.com>
2024-07-17 15:29:45 -0300

    Update - Conhecendo o JPQL

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.repository;
 import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +18,12 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByGenero(Categoria categoria);
 
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
+
+//    @Query(value = "select * from series WHERE series.total_temporadas <= 5 AND series.avaliacao >= 7.5", nativeQuery = true)
+//    List<Serie> seriesPorTemporadaEAValiacao(int totalTemporadas, double avaliacao);
+    // Não é tão recomendado trabalhar com native Query pois te deixa preso ao banco e não é dinãmico a respeito dos valores
+
+    //JPQL = Java Persistent Query Language | Linguagem de consultada da própria JPA do controle de persistência do Java
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
+    List<Serie> seriesPorTemporadaEAvaliacao(int totalTemporadas, double avaliacao);
 }
